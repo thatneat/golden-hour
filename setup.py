b5d7159dfa48b1fc8bd7cbeabfb329001c518e91
thatneat <thatneat@gmail.com>
2019-07-04 10:30:51 -0700

    add description for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+from os import path
 from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="golden_hour",
     version="1.0.0",
     author="Alan Hussey",
     description="Record a sunset timelapse and post it to Twitter with a weather report",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/alanhussey/golden-hour",
     packages=find_packages(),
     entry_points={
