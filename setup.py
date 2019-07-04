8ba934a56cfa92499b19a8738e4de49971aaca29
thatneat <thatneat@gmail.com>
2019-07-04 09:11:40 -0700

    better entry point name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url="https://github.com/alanhussey/golden-hour",
     packages=find_packages(),
     entry_points={
-        "console_scripts": ["goldenhour = goldenhour.main:main"]
+        "console_scripts": ["golden-hour = golden-hour.main:main"]
     },
     install_requires=[
         "astral==1.3.4",
