cc55724ca36f489a39b6026fb814121388a1a06e
thatneat <thatneat@gmail.com>
2019-07-29 22:46:38 -0700

    ok, so some places are actually below sea level

--- golden_hour/location.py
+++ golden_hour/location.py
@@ -1,6 +1,8 @@
 from astral import Astral, Location
 import schema
 
+# Lowest reasonable elevation is Death Valley, about 100m below sea level.
+LOWEST_ALLOWED_ELEVATION = -100
 
 LOCATION_CONFIG_SCHEMA = schema.Schema(
     # Location configuration can either be:
@@ -12,7 +14,7 @@ LOCATION_CONFIG_SCHEMA = schema.Schema(
             'latitude': schema.And(schema.Use(float), lambda lat: -90 < lat < 90),
             'longitude': schema.And(schema.Use(float), lambda lon: -180 < lon < 180),
             'timezone': str,
-            'elevation': schema.And(schema.Use(float), lambda elevation: elevation >= 0),
+            'elevation': schema.And(schema.Use(float), lambda elevation: elevation >= -LOWEST_ALLOWED_ELEVATION),
         }
     )
 )
